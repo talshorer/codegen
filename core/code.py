17cb204f41aed464186a1c897266cb916462826c
Tal Shorer <tal.shorer@gmail.com>
2015-08-13 14:10:56 +0300

    core: code: add empty_line
    Signed-off-by: Tal Shorer <tal.shorer@gmail.com>

--- core/code.py
+++ core/code.py
@@ -5,3 +5,12 @@ class Code(object):
 
     def _act(self, source):
         raise NotImplementedError("This is an abstract class")
+
+
+class _EmptyLine(object):
+
+    def _act(self, source):
+        source.linefeed()
+
+
+empty_line = _EmptyLine()
