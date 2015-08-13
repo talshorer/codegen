dd4ebe4e0cfb71225f456ce3b9a16e537e535dbe
Tal Shorer <tal.shorer@gmail.com>
2015-08-13 12:19:56 +0300

    lang: c: ccode: add Block class
    Signed-off-by: Tal Shorer <tal.shorer@gmail.com>

--- lang/c/ccode.py
+++ lang/c/ccode.py
@@ -17,3 +17,30 @@ class Expr(Code):
 
     def __repr__(self):
         return "{}({!r})".format(_cls_repr(type(self)), self.expr)
+
+    @classmethod
+    def exprs_from_text(cls, text):
+        return [cls(part.strip()) for part in text.split(";") if part]
+
+
+class Block(Code):
+
+    def __init__(self, code=None):
+        if code is None:
+            code = []
+        self.code = code
+
+    def add_code(self, code):
+        self.code.append(code)
+
+    def _act(self, source):
+        source.writeline("{")
+        source.indent()
+        for part in self.code:
+            part._act(source)
+            source.writeline(";")
+        source.dedent()
+        source.writeline("}")
+
+    def __repr__(self):
+        return "{}({!r})".format(_cls_repr(type(self)), self.code)
