a887cfa3dfe00db31f1cb9ae1b0eed3137bdc194
Tal Shorer <tal.shorer@gmail.com>
2015-08-13 12:51:02 +0300

    lang: c: ccode: add IfBlock class
    Signed-off-by: Tal Shorer <tal.shorer@gmail.com>

--- lang/c/ccode.py
+++ lang/c/ccode.py
@@ -38,12 +38,35 @@ class Block(Code):
         source.indent()
         for part in self.code:
             part._act(source)
-            source.writeline(";")
+            if isinstance(part, Expr):
+                source.writeline(";")
         source.dedent()
         source.writeline("}")
 
     def __repr__(self):
         return "{}({!r})".format(_cls_repr(type(self)), self.code)
+
+
+class IfBlock(Block):
+
+    MAGIC_WORD = "if"
+
+    def __init__(self, cond, code=None):
+        self.cond = cond
+        Block.__init__(self, code)
+
+    def _act(self, source):
+        source.write("{} (".format(self.MAGIC_WORD))
+        self.cond._act(source)
+        source.write(") ")
+        Block._act(self, source)
+
+    def __repr__(self):
+        return "{}({!r}, {!r})".format(
+            _cls_repr(type(self)),
+            self.decl,
+            self.code,
+        )
 
 
 class Func(Block):
