ce2cb3b51bd70b671c528bd3562d8564fb30e649
Tal Shorer <tal.shorer@gmail.com>
2015-08-13 12:13:06 +0300

    core: basic utilities
    Signed-off-by: Tal Shorer <tal.shorer@gmail.com>

---
+++ core/code.py
@@ -0,0 +1,7 @@
+#! /usr/bin/python3
+
+
+class Code(object):
+
+    def _act(self, source):
+        raise NotImplementedError("This is an abstract class")
---
+++ core/source.py
@@ -0,0 +1,37 @@
+#! /usr/bin/python3
+
+import collections
+
+SourceConfig = collections.namedtuple("SourceFileConfig", [
+    "stream",
+    "indentation",
+])
+
+
+class Source(object):
+
+    def __init__(self, config):
+        self.config = config
+        self._indent_level = 0
+        self._indented = False
+
+    def indent(self):
+        self._indent_level += 1
+
+    def dedent(self):
+        self._indent_level -= 1
+
+    def write(self, text):
+        if not self._indented:
+            self.config.stream.write(self._indent_level *
+                                     self.config.indentation)
+            self._indented = True
+        self.config.stream.write(text)
+
+    def linefeed(self):
+        self.config.stream.write("\n")
+        self._indented = False
+
+    def writeline(self, text):
+        self.write(text)
+        self.linefeed()
