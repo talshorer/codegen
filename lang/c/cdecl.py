b27ad7111a15506561753aa24b841258e5d0922a
Tal Shorer <tal.shorer@gmail.com>
2015-08-13 12:13:46 +0300

    lang: c: cdecl: added
    Allows creating declaration objects.
    
    Signed-off-by: Tal Shorer <tal.shorer@gmail.com>

---
+++ lang/c/cdecl.py
@@ -0,0 +1,68 @@
+#! /usr/bin/python3
+
+
+class CDecl(object):
+
+    def __init__(self, ctype, name):
+        self.ctype = ctype
+        self.name = name
+
+    def __str__(self):
+        return self.ctype._make(self.name)
+
+
+class _CType(object):
+
+    def _make(self):
+        raise NotImplementedError("This is an abstract class")
+
+
+class Primitive(_CType):
+
+    def __init__(self, typename):
+        self.typename = typename
+
+    def _make(self, decl):
+        return "{} {}".format(self.typename, decl)
+
+
+class NamelessArg(CDecl):
+
+    def __init__(self, ctype):
+        self.ctype = ctype
+
+    def __str__(self):
+        return self.ctype.typename
+
+
+void_args = (NamelessArg(Primitive("void")),)
+
+
+class Pointer(_CType):
+
+    def __init__(self, ptype):
+        self.ptype = ptype
+
+    def _make(self, decl):
+        return self.ptype._make("(*{})".format(decl))
+
+
+class Array(_CType):
+
+    def __init__(self, etype, size):
+        self.etype = etype
+        self.size = size
+
+    def _make(self, decl):
+        return self.etype._make("{}[{}]".format(decl, self.size))
+
+
+class Func(_CType):
+
+    def __init__(self, rettype, args):
+        self.rettype = rettype
+        self.args = args
+        self._args_str = ", ".join(str(a) for a in args)
+
+    def _make(self, decl):
+        return self.rettype._make("{}({})".format(decl, self._args_str))
