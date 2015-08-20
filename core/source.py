30c88309d5f7e6aa63838414f32f964a43295e9d
Tal Shorer <tal.shorer@gmail.com>
2015-08-20 11:55:14 +0300

    core: source: remove stream from config
    stream is now given when calling make, and the Source class has been
    split into two classes:
    	Source: holds elements and config
    	_SourceStream: generates code
    
    Signed-off-by: Tal Shorer <tal.shorer@gmail.com>

--- core/source.py
+++ core/source.py
@@ -3,19 +3,19 @@
 import collections
 
 SourceConfig = collections.namedtuple("SourceFileConfig", [
-    "stream",
     "indentation",
     "seperate_elements",
 ])
 
 
-class Source(object):
+class _SourceStream(object):
 
-    def __init__(self, config):
+    def __init__(self, config, stream, elements):
         self.config = config
+        self.stream = stream
+        self.elements = elements
         self._indent_level = 0
         self._indented = False
-        self._elements = []
 
     def indent(self):
         self._indent_level += 1
@@ -25,24 +25,34 @@ class Source(object):
 
     def write(self, text):
         if not self._indented:
-            self.config.stream.write(self._indent_level *
+            self.stream.write(self._indent_level *
                                      self.config.indentation)
             self._indented = True
-        self.config.stream.write(text)
+        self.stream.write(text)
 
     def linefeed(self):
-        self.config.stream.write("\n")
+        self.stream.write("\n")
         self._indented = False
 
     def writeline(self, text):
         self.write(text)
         self.linefeed()
 
-    def add_element(self, element):
-        self._elements.append(element)
-
-    def make(self):
-        for element in self._elements:
+    def __call__(self):
+        for element in self.elements:
             element._act(self)
             if self.config.seperate_elements:
                 self.linefeed()
+
+
+class Source(object):
+
+    def __init__(self, config):
+        self.config = config
+        self.elements = []
+
+    def add_element(self, element):
+        self.elements.append(element)
+
+    def make(self, stream):
+        return _SourceStream(self.config, stream, self.elements)()
