753e6a16f69d18ba60077bb2fedbe356276b341d
Tal Shorer <tal.shorer@gmail.com>
2015-08-20 12:56:31 +0300

    tests: lang: c: ccode: avoid splitting lines with no good reason
    Signed-off-by: Tal Shorer <tal.shorer@gmail.com>

--- tests/lang/c/test_ccode.py
+++ tests/lang/c/test_ccode.py
@@ -28,28 +28,21 @@ class TestExpr(CCodeTest):
         self.check_gen(dummy, "dummy", action="_act_with_parentheses")
 
     def test_parentheses_on_complex_expr(self):
-        self.check_gen(
-            ccode.Expr("0 + 1"),
-            "(0 + 1)",
-            action="_act_with_parentheses",
-        )
+        expr = ccode.Expr("0 + 1")
+        self.check_gen(expr, "(0 + 1)", action="_act_with_parentheses")
 
 
 class TestVariable(CCodeTest):
 
     def test_variable_name(self):
-        decl = ct_int("a")
-        self.check_gen(ccode.Variable(decl), "a")
+        self.check_gen(ccode.Variable(ct_int("a")), "a")
 
     def test_variable_name_with_initial(self):
-        decl = ct_int("a")
-        self.check_gen(ccode.Variable(decl, dummy), "a")
+        self.check_gen(ccode.Variable(ct_int("a"), dummy), "a")
 
     def test_variable_definition(self):
-        decl = ct_int("a")
-        self.check_gen(ccode.Variable(decl), "int a", action="_var_act")
+        self.check_gen(ccode.Variable(ct_int("a")), "int a", action="_var_act")
 
     def test_variable_definition_with_initial(self):
-        decl = ct_int("a")
-        var = ccode.Variable(decl, dummy)
+        var = ccode.Variable(ct_int("a"), dummy)
         self.check_gen(var, "int a = dummy", action="_var_act")
