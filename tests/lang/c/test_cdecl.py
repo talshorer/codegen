9580a66e97e221724d0aec103ce753200725f5f0
Tal Shorer <tal.shorer@gmail.com>
2015-08-20 12:34:33 +0300

    tests: lang: c: cdecl: add test for nameless args
    Signed-off-by: Tal Shorer <tal.shorer@gmail.com>

--- tests/lang/c/test_cdecl.py
+++ tests/lang/c/test_cdecl.py
@@ -47,6 +47,11 @@ class TestFunc(unittest.TestCase):
         decl = cdecl.Func(ct_int, [ct_int("b"), ct_int("c")])("a")
         self.assertEqual(str(decl), "int a(int b, int c)")
 
+    def test_func_with_nameless_args(self):
+        nameless_arg = cdecl.NamelessArg(ct_int)
+        decl = cdecl.Func(ct_int, [nameless_arg, nameless_arg])("a")
+        self.assertEqual(str(decl), "int a(int, int)")
+
 
 class TestArray(unittest.TestCase):
 
@@ -61,3 +66,25 @@ class TestArray(unittest.TestCase):
     def test_array_of_array(self):
         decl = cdecl.Array(cdecl.Array(ct_int, 1), 1)("a")
         self.assertEqual(str(decl), "int a[1][1]")
+
+
+class TestNamelessArg(unittest.TestCase):
+
+    def test_nameless_pointer(self):
+        decl = cdecl.NamelessArg(cdecl.Array(ct_int, 1))
+        self.assertEqual(str(decl), "int [1]")
+
+    def test_nameless_array(self):
+        decl = cdecl.NamelessArg(cdecl.Array(cdecl.Pointer(ct_int), 1))
+        self.assertEqual(str(decl), "int *[1]")
+
+    def test_nameless_pointer_to_func(self):
+        decl = cdecl.NamelessArg(cdecl.Pointer(cdecl.Func(
+            ct_int,
+            cdecl.void_args),
+        ))
+        self.assertEqual(str(decl), "int (*)(void)")
+
+    def test_nameless_pointer_to_array(self):
+        decl = cdecl.NamelessArg(cdecl.Pointer(cdecl.Array(ct_int, 1)))
+        self.assertEqual(str(decl), "int (*)[1]")
